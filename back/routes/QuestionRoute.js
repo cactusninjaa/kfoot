67dcbc71afcdea2d7135c40afa7aa5375be432ff
Anthony <anthonylopes037@gmail.com>
2024-02-29 16:02:55 +0100

    :sparkles: ADD : sparkles: route for QuestionRoute

--- back/routes/QuestionRoute.js
+++ back/routes/QuestionRoute.js
@@ -1,4 +1,9 @@
 import express from 'express'
 import { getQuestions, getQuestion } from '../controllers/QuestionController.js'
+import router from '../router.js'
 
 
+router.get('/', getQuestions)
+
+
+export default router
