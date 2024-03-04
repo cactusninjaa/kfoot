4d82a7a84de38d7a3e4a2e957468671e33b5080a
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2024-03-04 18:10:01 +0100

    :sparkles: UPGRADE :sparkles: view structure

---
+++ front/src/views/QuestionView.vue
@@ -0,0 +1,31 @@
+
+<script>
+export default {
+  data() {
+    return {
+      link: null,
+      question: null,
+      answer: null,
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/question/1')
+      .then((response) => response.json())
+      .then((data) => {
+        this.link = data.link;
+        this.question = data.question;
+        this.answer = data.answer;
+      })
+      .catch((error) => console.error('Erreur:', error));
+  },
+
+};
+</script>
+
+<template>
+  <div>
+    <p>{{ question }}</p>
+    <img v-if="this.link" :src="this.link" alt="Image de la question" />
+    <p>{{ answer }}</p>
+  </div>
+</template>
